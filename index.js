2ce96659ba434b5c4daf011f53f48f161ef42482
Simone Tasca <simo.tasca@gmail.com>
2023-09-21 13:45:17 +0200

    only fonts

--- index.js
+++ index.js
@@ -4,27 +4,24 @@ const fs = require("fs");
 const { glob } = require("glob");
 const { join } = require("path");
 
-console.log("ARGWW", process.argv)
-
 const fontsFolder = process.argv[2] || ".";
 const outputFolder = process.argv[3] || ".";
 const fontFaceBaseUrl = process.argv[4] || "/fonts";
 
-glob(join(fontsFolder, "/**/*"), { nodir: true }).then((matches) => {
+glob(join(fontsFolder, "/**/*.{ttf,otf}"), { nodir: true }).then((matches) => {
   const fontFaces = matches.map(m => m.replace(fontsFolder, "")).map(match => {
     let fileFolder = match.slice(0, match.lastIndexOf("/"));
     fileFolder = fileFolder.slice(fileFolder.lastIndexOf("/") + 1);
     if (fileFolder.startsWith("/_")) return "";
 
-    console.log(fileFolder);
-
     let fileName = match.slice(match.lastIndexOf("/") + 1);
     const extension = fileName.slice(fileName.lastIndexOf("."));
     fileName = fileName.replace(extension, "");
 
-    let [family, weight] = fileName.split("-");
-    let italic = weight.endsWith("Italic");
-    weight = weight.replace("Italic", "") || "Regular";
+    let [family, stringWeight] = fileName.split("-");
+    let italic = stringWeight.endsWith("Italic");
+    stringWeight = stringWeight.replace("Italic", "") || "Regular";
+    let weight = 400;
 
     switch (weight) {
       case "Thin":
@@ -59,10 +56,13 @@ glob(join(fontsFolder, "/**/*"), { nodir: true }).then((matches) => {
     const fontFace = `
 @font-face {
   font-family: "${family}";
-  src: url("${fontFaceBaseUrl + fileFolder}/${fileName + extension}");
+  src: url("${join(fontFaceBaseUrl, fileFolder, fileName + extension)}");
   font-weight: ${weight};
   font-style: ${italic ? "italic" : "normal"};
 }`;
+
+    console.log(`GENERATED: ${family} ${stringWeight} ${italic ? "Italic" : ""}`);
+
 
     return fontFace;
   });
